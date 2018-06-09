a6488d9f405329c5e2b78b386a57d972d61a0f8b
Frederico Pereira <frederico.silva.pereira@gmail.com>
2018-06-09 16:43:55 -0300

    Adds more specs to it

--- spec/requests/ubs_spec.rb
+++ spec/requests/ubs_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'ubs API', type: :request do
-  let!(:ubs) { create_list(:ubs, 10) }
+  let!(:ubs) { create_list(:ubs, 15) }
 
   describe 'GET /ubs' do
     before { get '/ubs' }
 
-    it 'returns a ubs list' do
+    it 'returns the total entries' do
+      json = JSON.parse(response.body)
       expect(json).not_to be_empty
-      expect(json.total_entries).to eq(10)
+      expect(json["total_entries"]).to eq(15)
+    end
+
+    it 'returns the defaults per page value' do
+      json = JSON.parse(response.body)
+      expect(json).not_to be_empty
+      expect(json["per_page"]).to eq(10)
+    end
+
+    it 'returns the exactly 10 ubs' do
+      json = JSON.parse(response.body)
+      expect(json).not_to be_empty
+      expect(json["entries"].size).to eq(10)
     end
 
     it 'returns status code 200' do
