ad7a4b8b7321146c82d9c427e6f0e33eb3ba22b6
Frederico Pereira <frederico.silva.pereira@gmail.com>
2018-06-09 17:42:15 -0300

    Adds the spec to find with specific point

--- spec/requests/ubs_spec.rb
+++ spec/requests/ubs_spec.rb
@@ -28,4 +28,28 @@ RSpec.describe 'ubs API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  context 'with a specific point in 10 meter of radius' do
+    describe 'GET /ubs?query=-23.60,-46.69' do
+      let!(:ubs_nexting) { create :ubs, latitude: -23.62, longitude: -46.63 }
+      
+      before { get '/ubs?query=-23.60,-46.69' }
+      
+      it 'returns the total entries' do
+        json = JSON.parse(response.body)
+        expect(json).not_to be_empty
+        expect(json["total_entries"]).to eq(1)
+      end
+      
+      it 'returns the exactly 1 ubs' do
+        json = JSON.parse(response.body)
+        expect(json).not_to be_empty
+        expect(json["entries"].size).to eq(1)
+      end
+      
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
